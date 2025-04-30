86b5766572dda9a41375d016876c5a22407876a2
yoland.nababan <yoland@code.id>
2025-04-30 16:40:45 +0700

    add crud for user

--- app/users/repository.go
+++ app/users/repository.go
@@ -2,7 +2,9 @@ package users
 
 import (
 	"log"
+	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -17,14 +19,54 @@ func NewUserRepo(db *gorm.DB) *repo {
 }
 
 type IUserRepository interface {
-	GetUsers() (out []*UserEntity, err error)
+	GetUsers() ([]*UserEntity, error)
+	GetUserByID(id uuid.UUID) (*UserEntity, error)
+	CreateUser(user *UserEntity) error
+	UpdateUser(user *UserEntity) error
+	DeleteUser(id uuid.UUID) error
 }
 
-func (r *repo) GetUsers() (out []*UserEntity, err error) {
-	err = r.DB.Find(&out).Error
+func (r *repo) GetUsers() ([]*UserEntity, error) {
+	var users []*UserEntity
+	err := r.DB.Find(&users).Error
 	if err != nil {
-		log.Println(err)
-		return
+		log.Println("GetUsers error:", err)
+		return nil, err
 	}
-	return
+	return users, nil
+}
+
+func (r *repo) GetUserByID(id uuid.UUID) (*UserEntity, error) {
+	var user UserEntity
+	err := r.DB.First(&user, "id = ?", id).Error
+	if err != nil {
+		log.Println("GetUserByID error:", err)
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *repo) CreateUser(user *UserEntity) error {
+	err := r.DB.Create(user).Error
+	if err != nil {
+		log.Println("CreateUser error:", err)
+	}
+	return err
+}
+
+func (r *repo) UpdateUser(user *UserEntity) error {
+	user.UpdatedAt = time.Now()
+	err := r.DB.Model(user).Updates(user).Error
+	if err != nil {
+		log.Println("UpdateUser error:", err)
+	}
+	return err
+}
+
+func (r *repo) DeleteUser(id uuid.UUID) error {
+	err := r.DB.Delete(&UserEntity{}, "id = ?", id).Error
+	if err != nil {
+		log.Println("DeleteUser error:", err)
+	}
+	return err
 }
